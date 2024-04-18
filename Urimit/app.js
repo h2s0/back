284e30084c3cb403798abfa463be4c98261d56dc
heeso <khs@alpinelab.io>
2024-04-18 16:12:57 +0900

    ♻️ 라우팅 분리 ( 따로 빼주기 )

--- Urimit/app.js
+++ Urimit/app.js
@@ -18,22 +18,25 @@
 
 
 
+"use strict";
+
 // express 를 사용하여 웹서버 띄우기
 
+// 모듈
 const express = require("express");
 const app = express();
+
+const PORT = 8000;
+
+// 라우팅
+const home = require("./routes/home");
 
 // 앱 세팅
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.render("home/index");
-});
-app.get("/login", (req, res) => {
-  res.render("home/login");
-});
+app.use("/", home); // use : 미들 웨어를 등록해주는 메서드
 
-app.listen(8000, ()=> {
+app.listen(PORT, ()=> {
   console.log("서버 가동");
 });
